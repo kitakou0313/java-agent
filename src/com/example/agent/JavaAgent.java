3e35df3f8c3435cdf47d918bb6b0634bf49202ea
kitakou0313 <kitakou0313@gmail.com>
2024-07-30 05:44:04 +0900

    add:define premain methods

--- src/com/example/agent/JavaAgent.java
+++ src/com/example/agent/JavaAgent.java
@@ -5,5 +5,11 @@ import java.lang.instrument.Instrumentation;
 public class JavaAgent {
     // javagent引数を用いて実行する場合，premainメソッドを定義する
     public static void premain(String agentArgs, Instrumentation instrumentation) {
+        System.out.println("[AGENT] In premain");
+
+        Class[] loadedClasses = instrumentation.getAllLoadedClasses();
+        for (int i = 0; i < loadedClasses.length; i++) {
+            System.out.println("[AGENT] This is loaded Class list:"+loadedClasses[i]);
+        }
     }
 }
