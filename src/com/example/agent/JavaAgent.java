c7058d55756e32ba3774ecbf1ef4d1f16d96b41f
kitakou0313 <kitakou0313@gmail.com>
2024-07-31 22:15:12 +0900

    log:change logger

--- src/com/example/agent/JavaAgent.java
+++ src/com/example/agent/JavaAgent.java
@@ -1,15 +1,18 @@
 package com.example.agent;
 
 import java.lang.instrument.Instrumentation;
+import java.util.logging.Logger;
 
 public class JavaAgent {
+    private static Logger logger = Logger.getLogger("Java agent");
+
     // javagent引数を用いて実行する場合，premainメソッドを定義する
     public static void premain(String agentArgs, Instrumentation instrumentation) {
-        System.out.println("[AGENT] In premain");
+        logger.info("[AGENT] In premain");
 
         Class[] loadedClasses = instrumentation.getAllLoadedClasses();
         for (int i = 0; i < loadedClasses.length; i++) {
-            System.out.println("[AGENT] This is loaded Class list:"+loadedClasses[i]);
+            logger.info("[AGENT] This is loaded Class list:"+loadedClasses[i]);
         }
     }
 }
