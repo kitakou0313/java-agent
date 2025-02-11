60b4b655f3ec2284619e37d7a326193255d514a6
kitakou0313 <kitakou0313@gmail.com>
2025-02-11 11:10:41 +0900

    add: add example without threadLocalValue class

--- src/com/example/threadLocalVal/Main.java
+++ src/com/example/threadLocalVal/Main.java
@@ -1,14 +1,20 @@
 package com.example.threadLocalVal;
 
 public class Main {
+    // 0で初期化
     private static ThreadLocal<Integer> threadLocalValue = ThreadLocal.withInitial(() -> 0);
+
+    private static Integer notThreadLocalValue = 0;
 
     public static void main(String[] args) {
         Runnable task = () -> {
+            // threadLocalValからRead -> 加算 -> 変数にwrite
             int value = threadLocalValue.get();
             value += 1;
             threadLocalValue.set(value);
             
+            // スレッド間で同じ変数からgetしているので，2になるケースがあるはず
+            // 実際はスレッドごとに値が分離されているので両スレッドで値が1になる
             System.out.println(
                 Thread.currentThread().getName() + ": Value of threadLocalValue :" + threadLocalValue.get()
             );
@@ -21,5 +27,23 @@ public class Main {
 
         thread1.start();
         thread2.start();
+
+        // ThreadLocalValueを使わない場合
+        Runnable taskWithoutThreadLocalValue = () -> {
+            Integer value = notThreadLocalValue;
+            value += 1;
+            notThreadLocalValue = value;
+
+            System.out.println(
+                Thread.currentThread().getName() + ": Value of threadLocalValue :" + notThreadLocalValue
+            );
+        };
+
+        Thread thread3 = new Thread(taskWithoutThreadLocalValue, "Thread-3");
+        Thread thread4 = new Thread(taskWithoutThreadLocalValue, "Thread-4");
+
+        thread3.start();
+        thread4.start();
+
     }
 }
